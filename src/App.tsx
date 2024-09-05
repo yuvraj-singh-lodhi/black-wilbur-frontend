f8f19257ab8ab97769fecb2e6217b4ffb4e68f12
RitanshRajput <it2042@global.org.in>
2024-09-05 18:47:47 +0530

    update_minor_changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Collection from "./components/collection";
 import Footer from "./components/footer";
 import Home from "./components/home";
 import Navbar from "./components/navbar";
@@ -6,7 +7,8 @@ function App() {
   return (
     <div>
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <Collection />
       <Footer />
     </div>
   );
